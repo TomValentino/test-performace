bd8a778a249edd65567f3f97d5d9fc0bf3d037ec
Tom Valentino <tommyvalentino420@gmail.com>
2026-06-08 17:58:52 +0700

    fd

--- src/lib/render.js
+++ src/lib/render.js
@@ -136,6 +136,8 @@ export const ColumnBlock = withBaseProps(function ColumnBlock({
         gap,
         alignItems:            align,
         justifyContent:        justify,
+                position: 'relative',
+
                 overflow: 'hidden',
 
         ...style,
